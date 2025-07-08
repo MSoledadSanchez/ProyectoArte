1157c403a245e555ed3d2cb0cffad66c942a461f
MSoledad <msoledad.sanchez@hotmail.com>
2025-07-08 17:16:58 -0300

    Trabajo
    Registro

---
+++ js/obras.js
@@ -0,0 +1,24 @@
+const productos= [
+{
+/* id: 1,
+titulo: "Remera New Balance",
+descripcion: "Dry Fit Color Verde",
+category: "Indumentaria",
+subcategory: "Remeras", 
+precio: "3500",
+image: "../img/remera1.jpg",
+cuotas: "3",
+descuento: "30" */
+
+id: 1,
+titulo: "Fuego en Colores",
+artista: "Juliana",
+imagenArt: "../media/Juliana.png",
+category: "Pintura",
+subcategory: "ver", 
+precio: "3500",
+image: "../media/pintura1.png",
+
+},
+
+]
